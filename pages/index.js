2691697482dd4d86d2be0820b1ea6a117aa08918
omgismartinez <martnzomg@gmail.com>
2022-05-09 22:20:40 -0500

    feat: UI temperature control

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,24 @@
 import Layout from '../components/Layout'
 
+const temp = 25
+
 export default function Home() {
   return (
     <Layout title={'Home'}>
       <div className='grid grid-rows-4 md:grid-rows-3 lg:grid-rows-2 grid-flow-col text-2xl sm:text-3xl gap-2 lg:gap-4 w-full'>
         <div className='p-8 row-span-2 col-span-2 md:col-span-1 gap-8 lg:gap-4 from-neutral-100 bg-gradient-to-t md:bg-gradient-to-tl lg:bg-gradient-to-l flex lg:justify-start justify-center rounded-lg'>
-          <div className='w-8 h-64 bg-blue-500 rounded-2xl'></div>
+          <div className={`w-8 h-64 rounded-2xl border-[1px] border-neutral-200`}>
+            <div className={`h-1/3 ${temp > 30 ? 'bg-gradient-to-t from-orange-500 to-red-500' : ''} rounded-t-2xl`}></div>
+            <div className={`h-1/3 ${temp >= 20 ? 'bg-gradient-to-t from-orange-500' : ''}
+                                   ${temp > 30 ? 'bg-gradient-to-b from-orange-500 to-orange-500' : ''}`}></div>
+            <div className={`h-1/3 ${temp < 20 ? 'bg-gradient-to-t from-blue-500' : ''} rounded-b-2xl
+                                   ${temp >= 20 ? 'bg-gradient-to-t from-blue-500 to-orange-500' : ''}`}></div>
+          </div>
           <div className='grid place-content-center'>
             <h1 className='text-center font-bold'>
               Temperatura
             </h1>
-            <h2 className='text-center text-neutral-400 font-bold'>{16} °C</h2>
+            <h2 className='text-center text-neutral-400 font-bold'>{temp} °C</h2>
           </div>
         </div>
         <div className='p-4 bg-neutral-100 grid place-content-center rounded-lg'>
