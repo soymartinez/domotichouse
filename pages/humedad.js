120ce1b824f81ddbb2cf2e45710a7bef4ca81f2c
omgismartinez <martnzomg@gmail.com>
2022-05-16 17:04:04 -0500

    add: template mqtt

--- pages/humedad.js
+++ pages/humedad.js
@@ -1,11 +1,28 @@
-import Layout from '../components/Layout';
+import Layout from 'components/layout';
+import { connBroker } from 'connections/mqtt';
 
 export default function Humedad() {
+    const [status, payload] = connBroker('domotic/humedad');
     return (
         <Layout title={'Humedad'}>
-            <h1 className='text-center text-3xl font-bold'>
-                Contenido de humedad
-            </h1>
+            {
+                status ?
+                    <div className='text-center text-3xl font-bold text-red-500'>
+                        🚀 Conectado
+                        <section className='p-4'>
+                            <h1 className='text-yellow-500'>
+                                Topic: {payload[0]}
+                            </h1>
+                            <h1 className='text-blue-500'>
+                                Humedad: {payload[1]}
+                            </h1>
+                        </section>
+                    </div>
+                    : <div className='text-center text-3xl font-bold text-neutral-400'>
+                        <i className='bx bx-loader bx-spin mr-2'></i>
+                        Cargando...
+                    </div>
+            }
         </Layout>
     );
 }
--- pages/temperatura.js
+++ pages/temperatura.js
@@ -1,11 +1,28 @@
-import Layout from '../components/Layout';
+import Layout from 'components/layout';
+import { connBroker } from 'connections/mqtt';
 
 export default function Temperatura() {
+    const [status, payload] = connBroker('domotic/temperatura');
     return (
         <Layout title={'Temperatura'}>
-            <h1 className='text-center text-3xl font-bold'>
-                Contenido de temperatura
-            </h1>
+            {
+                status ?
+                    <div className='text-center text-3xl font-bold text-red-500'>
+                        🚀 Conectado
+                        <section className='p-4'>
+                            <h1 className='text-yellow-500'>
+                                Topic: {payload[0]}
+                            </h1>
+                            <h1 className='text-blue-500'>
+                                Temperatura: {payload[1]}
+                            </h1>
+                        </section>
+                    </div>
+                    : <div className='text-center text-3xl font-bold text-neutral-400'>
+                        <i className='bx bx-loader bx-spin mr-2'></i>
+                        Cargando...
+                    </div>
+            }
         </Layout>
     );
 }
