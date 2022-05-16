e8922809cbfd35299fdd9d111cfb7f4400123e25
omgismartinez <martnzomg@gmail.com>
2022-05-16 17:08:29 -0500

    add: component logs

---
+++ pages/logs.js
@@ -0,0 +1,56 @@
+import Layout from "components/layout";
+
+export default function Logs({ data }) {
+    return (
+        <Layout title={'Logs'}>
+            <div className="font-bold md:text-xl w-full">
+                <div class="flex flex-col mt-2">
+                    <div class="overflow-x-auto scrollbar-thin scrollbar-thumb-neutral-400 scrollbar-track-neutral-200 md:rounded-lg rounded-md">
+                        <div class="inline-block min-w-full">
+                            <div class="overflow-hidden">
+                                <table className="min-w-full">
+                                    <thead className='bg-neutral-100'>
+                                        <tr>
+                                            <th className="py-3 px-6 text-sm font-medium text-left text-neutral-500
+                                                         uppercase tracking-wider">Date</th>
+                                            <th className="py-3 px-6 text-sm font-medium text-left text-neutral-500
+                                                         uppercase tracking-wider">Topic</th>
+                                            <th className="py-3 px-6 text-sm font-medium text-left text-neutral-500
+                                                         uppercase tracking-wider">Payload</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody className="">
+                                        {
+                                            data.map(({ _id, topic, message, createdAt }) => (
+                                                <tr key={_id} className='border-b'>
+                                                    <td className="text-neutral-400 py-4 px-6 whitespace-nowrap">{createdAt}</td>
+                                                    <td className="text-green-500 py-4 px-6 whitespace-nowrap">{topic}</td>
+                                                    <td className="text-blue-500 py-4 px-6 whitespace-nowrap flex justify-between">
+                                                        <span className='pr-2'>
+                                                            <span className='text-neutral-400'>temperatura:</span> {message.temperature}</span>
+                                                        <span className='pr-2'>
+                                                            <span className='text-neutral-400'>humedad:</span> {message.humidity}</span>
+                                                        <span>
+                                                            <span className='text-neutral-400'>aire:</span> {message.air}</span>
+                                                    </td>
+                                                </tr>
+                                            ))
+                                        }
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </Layout>
+    )
+}
+
+export async function getServerSideProps() {
+    const res = await fetch('http://localhost:3000/api/logs');
+    const data = await res.json();
+    return {
+        props: { data },
+    }
+}
