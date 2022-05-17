d1c533cda0fdd2e5ebf9ff630669d22c5d5e25e8
omgismartinez <martnzomg@gmail.com>
2022-05-16 21:03:11 -0500

    fix: broker protocol

--- connections/mqtt.js
+++ connections/mqtt.js
@@ -6,7 +6,7 @@ export function connBroker(topic) {
     const [payload, setPayload] = useState([]);
 
     useEffect(() => {
-        const client = mqtt.connect('ws://broker.emqx.io:8083/mqtt');
+        const client = mqtt.connect('wss://broker.emqx.io:8084/mqtt');
         client.on('connect', () => {
             client.subscribe(topic, (err) => {
                 setStatus(true)
