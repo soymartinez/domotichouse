093970726a5795f7054b81ea090962f5e6bef30c
omgismartinez <martnzomg@gmail.com>
2022-07-03 17:37:45 -0500

    refactor (models): schema log

--- connections/models/log.js
+++ connections/models/log.js
@@ -5,45 +5,13 @@ const logSchema = new Schema({
         type: String,
         required: [true, "Topic is required"],
         trim: true,
-        maxlength: [50, "Topic is too long"] 
+        maxlength: [50, "Topic is too long"]
     },
     message: {
-        type: Object,
+        type: Number,
         required: [true, "Message is required"],
-        properties: {
-            temperature: {
-                type: Number,
-                required: [true, "Temperature is required"],
-                min: [-30, "Temperature must be greater than -30"],
-                max: [50, "Temperature must be less than 50"],
-                title: "Temperature",
-                default: 0,
-                description: "Temperature in degrees Celsius",
-                unit: "°C",
-            },
-            humidity: {
-                type: Number,
-                required: [true, "Humidity is required"],
-                min: [0, "Humidity must be greater than 0"],
-                max: [100, "Humidity must be less than 100"],
-                title: "Humidity",
-                default: 0,
-                description: "Humidity in percentage",
-                example: 0,
-                unit: "%",
-            },
-            air: {
-                type: Number,
-                required: [true, "Air is required"],
-                min: [0, "Air must be greater than 0"],
-                max: [100, "Air must be less than 100"],
-                title: "Air",
-                default: 0,
-                description: "Air in percentage",
-                example: 0,
-                unit: "%",
-            },
-        }
+        trim: true,
+        maxlength: [50, "Message is too long"]
     }
 }, {
     timestamps: true,
