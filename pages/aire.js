136fcb9ad5982db44e7805c4d7b5fd00dead2b76
omgismartinez <martnzomg@gmail.com>
2022-07-03 17:38:41 -0500

    refactor (aire): page aire whit table

--- pages/aire.js
+++ pages/aire.js
@@ -1,28 +1,10 @@
 import Layout from 'components/layout';
-import { connBroker } from 'connections/mqtt';
+import Table from 'components/table';
 
 export default function Aire() {
-    const [status, payload] = connBroker('domotic/aire');
     return (
         <Layout title={'Aire'}>
-            {
-                status ?
-                    <div className='text-center text-3xl font-bold text-red-500'>
-                        🚀 Conectado
-                        <section className='p-4'>
-                            <h1 className='text-yellow-500'>
-                                Topic: {payload[0]}
-                            </h1>
-                            <h1 className='text-blue-500'>
-                                Aire: {payload[1]}
-                            </h1>
-                        </section>
-                    </div>
-                    : <div className='text-center text-3xl font-bold text-neutral-400'>
-                        <i className='bx bx-loader bx-spin mr-2'></i>
-                        Cargando...
-                    </div>
-            }
+            <Table value={'air'} sign={'%'} />
         </Layout>
     );
 }
